a9ed73b5fe8b0a1f28e7081c3e525a818d4af7dd
Alsace Cablar <cablaralsace@gmail.com>
2021-06-07 19:18:14 +0800

    main: objects.js

--- js-challenges-and-projects-v.1.2/es6/coding-challenges/objects.js
+++ js-challenges-and-projects-v.1.2/es6/coding-challenges/objects.js
@@ -42,65 +42,86 @@ var store = {
   earnings: 0,
 };
 
-// var book = [
-//   {
-//     title: "Twilight",
-//     quantity: 100,
-//     value: 600,
-//   },
-// ];
+var Book = function (title, quantity, value) {
+  let bookInformation = {};
+  bookInformation.title = title;
+  bookInformation.quantity = quantity;
+  bookInformation.value = value;
 
-// var addBook = function (title, quantity, value) {
-//   let bookInformation = {};
-//   bookInformation.title = title;
-//   bookInformation.quantity = quantity;
-//   bookInformation.value = value;
-//   store.inventoryList.push(bookInformation);
+  this.addBook = function () {
+    store.inventoryList.push(bookInformation);
+  };
 
-//   this.restockBook = function () {
-//     // for (let i = 0; i < store.inventoryList.length; i++) {
-//     if (bookInformation.quantity > 0) {
-//       console.log(
-//         `You still have ${bookInformation.quantity} pieces of ${bookInformation.title} in the inventory.`
-//       );
-//     } else {
-//       console.log(`Please restock ${bookInformation.title}.`);
-//     }
-//   };
-//   // };
+  // this.checkStock = function () {
+  //   // for (let i = 0; i < store.inventoryList.length; i++) {
+  //   if (bookInformation.quantity > 0) {
+  //     console.log(
+  //       `You still have ${bookInformation.quantity} pieces of ${bookInformation.title} in the inventory.`
+  //     );
+  //   } else {
+  //     console.log(`Please restock ${bookInformation.title}.`);
+  //   }
+  // };
+  // };
 
-//   this.sellBook = function () {
-//     //search for book
-//     //deduct quantity
-//     //add earnings
-//     let element = store.inventoryList.find((el) => el.title === title);
-//     if (element.quantity > 0) {
-//       console.log(`Only ${element.quantity} stocks left.`);
-//     }
-//     element.quantity -= quantity;
-//     store.earnings += quantity * element.value;
-//     console.log(
-//       `Successful Transaction! There are still ${element.quantity} stocks of ${bookInformation.title} left.`
-//     );
-//   };
+  // this.restockBook = function (title, quantity) {
+  //   for (let i = 0; i < store.inventoryList.length; i++) {
+  //     if (store.inventoryList[i].title === title) {
+  //       store.inventoryList[i].quantity += quantity;
+  //     }
+  //   }
+  // };
+};
 
-//   this.listInventory = function () {
-//     console.log(`${store.inventoryList}`);
-//   };
-// };
+function restockBook(title, quantity) {
+  for (let i = 0; i < store.inventoryList.length; i++) {
+    if (store.inventoryList[i].title === title) {
+      store.inventoryList[i].quantity += quantity;
+    }
+  }
+}
 
-// function totalEarnings() {
-//   console.log(`${store.storeName} has P${store.earnings} total earnings.`);
-// }
+function sellBook(title, quantity) {
+  let element = store.inventoryList.find((el) => el.title === title);
+  if (element.quantity < quantity) {
+    console.log(`Sorry, ${element.title} is currently out of stock.`);
+    return;
+  }
+  element.quantity -= quantity;
+  store.earnings += quantity * element.value;
+  console.log(
+    `Successful Transaction! There are now ${element.quantity} stocks of ${element.title} left.`
+  );
+}
 
-// let twilight = new addBook("Twilight", 0, 500);
-// let newMoon = new addBook("New Moon", 100, 500);
-// // let eclipse = new addBook("Eclipse", 300, 500);
-// let buyNewMoon = new addBook("New Moon", 3);
+function listInventory() {
+  for (let i = 0; i < store.inventoryList.length; i++) {
+    console.log(
+      `[Title: ${store.inventoryList[i].title}, Stocks left: ${store.inventoryList[i].quantity}]`
+    );
+  }
+}
 
-// // console.log(book);
-// twilight.restockBook();
+function totalEarnings() {
+  console.log(`${store.storeName} has P${store.earnings} total earnings.`);
+}
+
+let twilight = new Book("Twilight", 0, 500);
+let newMoon = new Book("New Moon", 100, 500);
+let eclipse = new Book("Eclipse", 50, 500);
+
+twilight.addBook();
+newMoon.addBook();
+eclipse.addBook();
+
+restockBook("New Moon", 300);
+restockBook("Eclipse", 100);
 // newMoon.restockBook();
-// buyNewMoon.sellBook();
-// totalEarnings();
-// console.log(store.listInventory());
+// eclipse.restockBook();
+
+sellBook("Twilight", 1);
+sellBook("New Moon", 1);
+sellBook("Eclipse", 3);
+
+listInventory();
+totalEarnings();
